72de786e081e385d9b850952a4173d2c59c2d3db
Bilal ARKAN <simbolmina@hotmail.com>
2022-04-24 20:20:31 +0300

    added read me

--- app.js
+++ app.js
@@ -178,24 +178,3 @@ app.all('*', (req, res, next) => {
 app.use(globalErrorHandler);
 
 module.exports = app;
-
-/* some suggestions
-
-business logic - api side
-1- add restriction that user can only review tours they are booked
-2- nested booking routes. like all and for invidual user
-3- imporove tour dates. if certean tour reaches maxGroup size\ suggest another date to new user who wants to buy that tour. participant count should not booked ones.
-4-confirm user mail address, add refresh token feature and two-factor auth
-
-website
-1- signup page
-2- add review option for user who booked / participated tour
-3- add "like tour" function for all user
-4- hide booking section from tour page from user who are already booked or add a message as book again
-5- add 'my reviews' page and user can edit there
-6- for admins, create a manage page. they can create, read, update, delete tours, users, reviews and bookings.
-
-
-
-
-*/
