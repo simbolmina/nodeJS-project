57cf837b53bfb29fe79f1115ab203db18249e7e4
Bilal ARKAN <simbolmina@hotmail.com>
2022-04-25 13:47:28 +0300

    improved strip implemention

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -30,7 +30,9 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
         name: `${tour.name} Tour`,
         description: tour.summary,
         //   images: [`https://www.natours.dev/img/tours/${tour.imageCover}.jpg`]
-        images: ['https://www.natours.dev/img/tours/tour-2-cover.jpg'],
+        images: [
+          `${req.protocol}://${req.get('host')}/img/tours/${tour.imageCover}`,
+        ],
         amount: tour.price * 100, //amount is expected in cents
         currency: 'usd',
         quantity: 1,
@@ -58,7 +60,7 @@ const createBookingCheckout = async (session) => {
   //we are getting these info from getCheckoutSession function and session variable.
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
 
   await Booking.create({ tour, user, price });
 };
@@ -77,7 +79,7 @@ exports.webhookCheckout = (req, res, next) => {
     return res.status(400).send(`webhook error: ${err.meesage}`);
   }
 
-  if (event.type === 'checkout.session.complete')
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
